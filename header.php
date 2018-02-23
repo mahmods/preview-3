10a74a4b0bdcbe6ac909e28a08dc4d210d9ed565
Mahmoud Slah <mahmodslah25@gmail.com>
2018-02-23 19:22:14 +0200

    init

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <base href="http://localhost:8080/skin/">
+    <base href="https://preview3-mahmoud.herokuapp.com/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
